c79e43b2c267aeef86c426204b578f0d1dee5c53
sophusweidemann <sophusaugustweidemann@gmail.com>
2018-02-12 15:48:44 +0100

    nrk.no/HideApartments.user.js | Remove jQuery, rewrite in vanilla js

--- nrk.no/HideApartments.user.js
+++ nrk.no/HideApartments.user.js
@@ -1,24 +1,45 @@
 // ==UserScript==
 // @name        NRK.no | Hide "apartments"
-// @version     1.1
+// @version     2
 // @include     https://*.nrk.no/
 // @author      h
 // @grant       none
-// @require     https://code.jquery.com/jquery-3.3.1.min.js
 // @run-at      document-end
 // @downloadURL https://raw.githubusercontent.com/HxxxxxS/Userscripts/master/nrk.no/HideApartments.user.js
 // ==/UserScript==
 
-jQuery(document).ready(function($){
-  
-  $('head').append('<style>.kur-floor--apartment.aHidden{ padding-bottom:0; }')
-  $('head').append('<style>.kur-floor--apartment.aHidden div.kur-floor__content div{ display:none; }')
-  $('head').append('<style>.kur-floor--apartment.aHidden h2.kur-floor__title:after{ margin-left:15px; color: #dadada; content: "(Click to read)" }')
+(function(){
 
-  $('.kur-floor--apartment:has(h2.kur-floor__title)').addClass('aHidden');
+  'use strict';
 
-  $('.kur-floor--apartment').on('click', function(){
-    $(this).toggleClass('aHidden');
-  });
-  
-});
+  function appendStyle(styles) {
+    var css = document.createElement('style');
+    css.type = 'text/css';
+
+    if (css.styleSheet) css.styleSheet.cssText = styles;
+    else css.appendChild(document.createTextNode(styles));
+
+    document.getElementsByTagName("head")[0].appendChild(css);
+  }
+
+  var styles  = '.kur-floor--apartment.aHidden{ padding-bottom:0; }';
+      styles += '.kur-floor--apartment.aHidden div.kur-floor__content div{ display:none; }';
+      styles += '.kur-floor--apartment.aHidden h2.kur-floor__title:after{ margin-left:15px; color: #dadada; content: "(Click to read)" }';
+
+  window.onload = function() { appendStyle(styles) };
+
+  var apts = document.getElementsByClassName('kur-floor--apartment');
+
+  for (var i=0;i<apts.length;i++) {
+
+    var apt = apts[i];
+
+    apt.className += ' aHidden';
+
+    apt.addEventListener("click", function(){
+        this.classList.toggle('aHidden');
+    });
+
+  }
+
+})();
